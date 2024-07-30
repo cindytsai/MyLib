d2d6217d40def8da3f337e01032ce5ff04d35c0e
cindytsai <turquoisea.tsai@gmail.com>
2024-07-30 16:44:59 -0500

    pybind adds current dir to path.

--- src/PyBind11/PyBind11Demo.cpp
+++ src/PyBind11/PyBind11Demo.cpp
@@ -17,7 +17,7 @@ int PyBind11Finalize() {
 
 int PyBind11CallTestScript() {
     std::cout << "====================" << std::endl;
-    pybind11::exec("import sys; sys.path.insert(0, '.'); help('modules')");
+    pybind11::exec("import sys;");
     pybind11::exec("import test_script; test_script.test_func()");
     pybind11::exec("print(dir())");
     std::cout << "====================" << std::endl;
