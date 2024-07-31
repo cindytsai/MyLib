f88f9667762f45fb7e921aff053d8431d8ce6c9e
cindytsai <turquoisea.tsai@gmail.com>
2024-07-31 15:44:16 -0500

    Adding objects.

--- src/PyBind11/PyBind11Demo.cpp
+++ src/PyBind11/PyBind11Demo.cpp
@@ -15,8 +15,9 @@ int PyBind11Finalize() {
     return 0;
 }
 
-PYBIND11_EMBEDDED_MODULE(fast_calc, m) {
-    // `m` is a `py::module_` which is used to bind functions and classes
+PYBIND11_EMBEDDED_MODULE(pybind11_libyt, m) {
+    using namespace pybind11::literals; // to bring in the `_a` literal
+    m.attr("hierarchy") = pybind11::dict("spam"_a=pybind11::none());
     m.def("add", [](int i, int j) {
         return i + j;
     });
@@ -24,15 +25,24 @@ PYBIND11_EMBEDDED_MODULE(fast_calc, m) {
 
 int PyBind11CallTestScript() {
     std::cout << "====================" << std::endl;
-    pybind11::exec("import sys;");
+    pybind11::exec("import sys; print(sys.version)");
     pybind11::exec("import test_script; test_script.test_func()");
     pybind11::exec("print(dir())");
     pybind11::exec("print(locals())");
     std::cout << "====================" << std::endl;
-    auto fast_calc = pybind11::module_::import("fast_calc");
-    auto result = fast_calc.attr("add")(1, 2).cast<int>();
-    std::cout << result << std::endl; // should be 3
+    auto pybind11_libyt = pybind11::module_::import("pybind11_libyt");
+    pybind11::exec("print(dir())"); // does not show pybind11_libyt
     std::cout << "====================" << std::endl;
+    pybind11::exec("import pybind11_libyt");
+    pybind11::exec("print(dir())"); // showing pybind11_libyt
+    std::cout << "====================" << std::endl;
+    pybind11::exec("print(dir(pybind11_libyt))");
+    pybind11::exec("print(pybind11_libyt.hierarchy)");
+    auto hier = pybind11_libyt.attr("hierarchy");
+    hier["spam"] = 1;
+    pybind11::exec("print(pybind11_libyt.hierarchy)");
+    pybind11::object result = pybind11_libyt.attr("add")(1, 2);
+    std::cout << "call pybind11_libyt.add(1,2) = " << result.cast<int>() << std::endl;
     return 0;
 }
 
