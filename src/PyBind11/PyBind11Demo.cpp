43fe0954213338d301cf2535c578dbbb3f3b8b25
cindytsai <turquoisea.tsai@gmail.com>
2024-08-01 12:49:34 -0500

    Appending data.

--- src/PyBind11/PyBind11Demo.cpp
+++ src/PyBind11/PyBind11Demo.cpp
@@ -1,5 +1,6 @@
 #ifdef USE_PYBIND11
 
+#include "DataTypes/YTParameters.h"
 #include "pybind11/embed.h"
 #include <iostream>
 
@@ -10,18 +11,13 @@ int PyBind11Initialize(const char *inline_script) {
     std::cout << "[PyBind11Initialize] Using Python " << std::endl;
     pybind11::exec("import sys; print(sys.version)");
     pybind11::exec("import " + std::string(inline_script));
-    std::cout << "[PyBind11Initialize] import " << inline_script << std::endl;
+    pybind11::exec("import pybind11_libyt");
+    std::cout << "[PyBind11Initialize] import " << inline_script << " and pybind11_libyt" << std::endl;
     return 0;
 }
 
 int PyBind11Finalize() {
     pybind11::finalize_interpreter();
-    return 0;
-}
-
-int PyBind11SetParameters(struct yt_param_yt *yt_param_ptr) {
-    auto pybind11_libyt = pybind11::module_::import("pybind11_libyt");
-    pybind11::dict hier = pybind11_libyt.attr("param_yt");
     return 0;
 }
 
@@ -36,7 +32,23 @@ PYBIND11_EMBEDDED_MODULE(pybind11_libyt, m) {
     });
 }
 
+int PyBind11SetParameters(struct yt_param_yt *yt_param_ptr) {
+    std::cout << "[PyBind11] Setting parameter" << std::endl;
+
+    auto pybind11_libyt = pybind11::module_::import("pybind11_libyt");
+    pybind11::dict hier = pybind11_libyt.attr("param_yt");
+
+    hier["frontend"] = 1;
+
+    pybind11::exec("print(pybind11_libyt.param_yt)");
+
+    return 0;
+}
+
 int PyBind11CallTestScript() {
+    /*
+     * Call in example.cpp only
+     */
     std::cout << "====================" << std::endl;
     auto pybind11_libyt = pybind11::module_::import("pybind11_libyt");
     pybind11::exec("print(dir())"); // does not show pybind11_libyt
